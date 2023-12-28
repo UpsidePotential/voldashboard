444c257472c7aaa61a804292723d79ecd9cce33a
mafuchs <mikeafruchs@gmail.com>
2023-12-28 11:24:55 -0600

    get better errors

--- src/services/marketData.ts
+++ src/services/marketData.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import os from "os";
+
 const TradingView = require('@mathieuc/tradingview');
 
 interface Quote {
@@ -36,11 +39,39 @@ export class MarketData {
     }
 
     async login(): Promise<void> {
+      const remember = false;
+      const validateStatus = (status: any) => status < 500;
+      const UA = 'TWAPI/3.0'
       try {
-        const user = await TradingView.loginUser(encodeURIComponent(process.env.TV_USER), encodeURIComponent(process.env.TV_PASS), false);
-        console.log('User:', user);
-        console.log('Sessionid:', user.session);
-        console.log('Signature:', user.signature);
+        const { data, headers } = await axios.post(
+          'https://www.tradingview.com/accounts/signin/',
+          `username=${encodeURIComponent(process.env.TV_USER)}&password=${encodeURIComponent(process.env.TV_PASS)}${remember ? '&remember=on' : ''}`,
+          {
+            validateStatus,
+            headers: {
+              referer: 'https://www.tradingview.com',
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'User-agent': `${UA} (${os.version()}; ${os.platform()}; ${os.arch()})`,
+            },
+          },
+        );
+
+        console.log(headers);
+        console.log(data);
+    
+        const cookies = headers['set-cookie'];
+    
+        if (data.error) throw new Error(data.error);
+    
+        const sessionCookie = cookies.find((c) => c.includes('sessionid='));
+        const session = (sessionCookie.match(/sessionid=(.*?);/) ?? [])[1];
+    
+        const signCookie = cookies.find((c) => c.includes('sessionid_sign='));
+        const signature = (signCookie.match(/sessionid_sign=(.*?);/) ?? [])[1];
+
+        console.log(`session: ${session} sig: ${signature}`)
+
+
       } catch(e) {
         console.log(e);
       }
