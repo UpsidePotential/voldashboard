ce22c651696b0be1fba38074421b84effac0f28b
mafuchs <mikeafruchs@gmail.com>
2024-04-20 15:57:36 -0500

    show live market data

--- src/routes/vx30.ts
+++ src/routes/vx30.ts
@@ -1,11 +1,29 @@
 import { Router } from 'express';
-import { VX30MarketData } from '../vix';
+import { LiveData, VX30MarketData } from '../vix';
 
 
 export const vx30 = Router();
 
 vx30.get('/vx30', async (req, res) => {
 
+    const livemarketdata = await LiveData();
     const newData = await VX30MarketData();
-    res.render('vx30', {data: {vx30: newData}});
+
+    function convertTime(time: any) {
+        // Check if the time is already in milliseconds
+        if (time.toString().length === 13) {
+          return new Date(time);
+        } else {
+          // Convert seconds to milliseconds
+          return new Date(time * 1000);
+        }
+      }
+
+    const dataArray = Object.keys(livemarketdata).map(symbol => ({
+        symbol,
+        close: livemarketdata[symbol].close,
+        time: convertTime(livemarketdata[symbol].time)
+      }));
+
+    res.render('vx30', {data: {vx30: newData, livedata: dataArray}});
 })
