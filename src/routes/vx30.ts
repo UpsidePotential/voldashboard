e061d3a47718b7d2e13aa73d7b581b8ded0114e7
mafuchs <mikeafruchs@gmail.com>
2024-04-20 15:06:24 -0500

    add raw data

--- src/routes/vx30.ts
+++ src/routes/vx30.ts
@@ -1,18 +1,11 @@
 import { Router } from 'express';
-import { VXEntryModel } from '../vxModel'
-import { buildVXData, FuturesContract} from '../vix';
+import { VX30MarketData } from '../vix';
 
 
 export const vx30 = Router();
 
 vx30.get('/vx30', async (req, res) => {
-    const vx_sum = await VXEntryModel.find().exec()
 
-    const newData = await buildVXData(req.app.locals.marketData, vx_sum);
-
-    const data = vx_sum.map( value => {
-       return [new Date(value.date).getTime(), Number(value.premium_zscore)];
-    });
-
-    res.render('vx30', {data: {latest: newData, historical: data.slice(data.length-300)} });
+    const newData = await VX30MarketData();
+    res.render('vx30', {data: {vx30: newData}});
 })
